701db06bd39697bc94879f839d5118f5d04014a9
JBB <980374208@qq.com>
2023-08-16 23:21:29 +0800

    add

--- openGl/sample_1/Shader/uniformLayout/main.cpp
+++ openGl/sample_1/Shader/uniformLayout/main.cpp
@@ -1,41 +1,32 @@
 ﻿#include <glad/glad.h> 
 #include <GLFW/glfw3.h>
 #include <iostream>
-
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"out vec4 vertexColor; \n"
-"void main()\n"
-"{\n"
-" FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
-
-const char* fragmentShaderSource2 = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"in vec4 vertexColor;\n"
-"void main()\n"
-"{\n"
-" FragColor = vec4(1.0f, 0.7f, 0.9f, 1.0f);\n"
-"}\n\0";
-
-float vertices[] = {
--0.9f, -0.5f, 0.0f, // left 
--0.0f, -0.5f, 0.0f, // right 
--0.45f, 0.5f, 0.0f, // top 
-0.0f, -0.5f, 0.0f, // left 
-0.9f, -0.5f, 0.0f, // right 
-0.45f, 0.5f, 0.0f // top 
-};
-
-unsigned int indices[] = { // 注意，我们从零开始算! 
-0, 1, 3, // 第一个三角形 
-1, 2, 3 // 第二个三角形 
-};
-
+#include "cmath"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
+const char* vertexShaderSource = "#version 330 core\n"
+"layout(location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(aPos, 1.0);\n"
+"}\n\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"uniform vec4 ourColor;\n"
+"void main()\n"
+"{\n"
+"FragColor = ourColor;\n"
+"}\n\0";
+
+float vertices[] = {
+-0.5f, -0.5f, 0.0f,
+0.5f, -0.5f, 0.0f,
+0.0f, 0.5f, 0.0f
+};
+
 
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -56,11 +47,11 @@ int main() {
 		glfwTerminate();
 		return -1;
 	}
+	//注册重绘回调函数
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
 	//GLFW将窗口的上下文设置为当前线程的上下文
 	glfwMakeContextCurrent(window);
-
-		//注册重绘回调函数
-		glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	//GLAD
 	// glad: 加载所有OpenGL函数指针
@@ -68,10 +59,20 @@ int main() {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-
 	//glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+
+
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// 检查编译错误
 	int success;
 	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
 	// 片段着色器
 	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
@@ -84,6 +85,7 @@ int main() {
 	}
 	//着色器程序
 	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
 	//链接错误检查
@@ -92,57 +94,23 @@ int main() {
 		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
 	}
+	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	// 片段着色器
-	unsigned int fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
-	glCompileShader(fragmentShader2);
-	// 检查编译错误
-	glGetShaderiv(fragmentShader2, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(fragmentShader2, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-	//着色器程序
-	unsigned int shaderProgram2 = glCreateProgram();
-	glAttachShader(shaderProgram2, fragmentShader2);
-	glLinkProgram(shaderProgram2);
-	//链接错误检查
-	glGetProgramiv(shaderProgram2, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram2, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-	}
-	glDeleteShader(fragmentShader2);
-	
-
-	unsigned int VBOs[2], VAOs[2]/*, EBO*/;
-	glGenVertexArrays(2, VAOs);
-	glGenBuffers(2, VBOs);
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
 	//绑定VBO和VAO对象
-	glBindVertexArray(VBOs[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	//为当前绑定到target的缓冲区对象创建一个新的数据存储。
 	//如果data不是NULL，则使用来自此指针的数据初始化数据存储
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices)/2/*缓冲区大小*/, vertices/*从哪个位置开始取值*/, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 	//告知Shader如何解析缓冲里的属性值
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-
 	//开启VAO管理的第一个属性值
 	glEnableVertexAttribArray(0);
-
-	glBindVertexArray(VBOs[1]);
-	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-	//为当前绑定到target的缓冲区对象创建一个新的数据存储。
-	//如果data不是NULL，则使用来自此指针的数据初始化数据存储
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices[9], GL_STATIC_DRAW);
-	//告知Shader如何解析缓冲里的属性值
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-
-	//开启VAO管理的第一个属性值
-	glEnableVertexAttribArray(0);
-
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
@@ -154,13 +122,14 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); //状态设置
 		glClear(GL_COLOR_BUFFER_BIT); //状态使用
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VBOs[0]);
-		glDrawArrays(GL_TRIANGLES,0,3);
-		//glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
 
-		glUseProgram(shaderProgram2);
-		glBindVertexArray(VBOs[1]);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		float timeValue = glfwGetTime();
+		float greenValue = sin(timeValue) / 2.0f + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES,0,3);
 
 		// glfw: 交换缓冲区和轮询IO事件（按键按下/释放、鼠标移动等）
 		glfwSwapBuffers(window);
@@ -168,9 +137,8 @@ int main() {
 	}
 	// glfw: 回收前面分配的GLFW先关资源. 
 	glfwTerminate();
-	glDeleteVertexArrays(2,VAOs);
-	glDeleteBuffers(2,VBOs);
-
-	
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	//glDeleteBuffers(1, &EBO);
 	return 0;
 }
